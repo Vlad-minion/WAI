7e89fe15d74606c9301b1455b1cf05dad5ba8b7c
Vlad-minion <vladpopkov1703@gmail.com>
2022-08-07 06:15:27 +0800

    mobile commit 19

--- Js/index.js
+++ Js/index.js
@@ -15,3 +15,19 @@ menuBtn.addEventListener("click", function (evt) {
   menuBtn.classList.toggle("menu-btn_active");
  menuActive.classList.toggle("menu_nav_active")
 });
+
+
+const anchors = document.querySelectorAll("a.scroll-to");
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href");
+
+    document.querySelector(author_page).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
